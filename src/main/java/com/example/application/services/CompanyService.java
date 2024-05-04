9a963f985db86a872182c850a998f6a12530ae9d
Yozhef <satwoodt@gmail.com>
2024-05-04 18:37:53 +0200

    first deploy

--- src/main/java/com/example/application/services/CompanyService.java
+++ src/main/java/com/example/application/services/CompanyService.java
@@ -1,13 +1,7 @@
 package com.example.application.services;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class CompanyService {
@@ -24,21 +18,30 @@ public class CompanyService {
     // return restTemplate.getForObject(url, String.class);
     // }
 
-    public String getData() {
-        String filePath = "/home/yozhef/Documents/response.json";
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        try {
-            File file = new File(filePath);
-            if (!file.exists()) {
-                return "Файл не найден: " + filePath;
-            }
-
-            Object jsonData = objectMapper.readValue(file, Object.class);
-            return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonData);
-
-        } catch (IOException e) {
-            return "Ошибка при чтении файла: " + e.getMessage();
-        }
+    public String getData(String InnParam) {
+        String apiKey = "db28411f27f2b1b286468ede0ed4f2fc30a4c5a8";
+        String sections = "fns[1,2,4,9,10],bals,checks,isps[1],arbs[1],scoring";
+        String url = String.format("https://api.damia.ru/spk/report?req=%s&key=%s&format=json&sections=%s", InnParam,
+                apiKey, sections);
+        return restTemplate.getForObject(url, String.class);
     }
+
+    // public String getData() {
+    // String filePath = "/home/yozhef/Documents/response.json";
+    // ObjectMapper objectMapper = new ObjectMapper();
+
+    // try {
+    // File file = new File(filePath);
+    // if (!file.exists()) {
+    // return "Файл не найден: " + filePath;
+    // }
+
+    // Object jsonData = objectMapper.readValue(file, Object.class);
+    // return
+    // objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonData);
+
+    // } catch (IOException e) {
+    // return "Ошибка при чтении файла: " + e.getMessage();
+    // }
+    // }
 }
--- src/main/java/com/example/application/views/searchforacompany/SearchforacompanyView.java
+++ src/main/java/com/example/application/views/searchforacompany/SearchforacompanyView.java
@@ -1,60 +1,23 @@
 package com.example.application.views.searchforacompany;
 
 import com.example.application.data.JSONDataDisplay;
-import com.example.application.data.SamplePerson;
 import com.example.application.services.CompanyService;
-import com.example.application.services.SamplePersonService;
 import com.example.application.views.MainLayout;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Key;
-import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.accordion.Accordion;
-import com.vaadin.flow.component.accordion.AccordionPanel;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.button.ButtonVariant;
-import com.vaadin.flow.component.checkbox.CheckboxGroup;
-import com.vaadin.flow.component.combobox.MultiSelectComboBox;
-import com.vaadin.flow.component.datepicker.DatePicker;
-import com.vaadin.flow.component.dependency.Uses;
-import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.grid.GridVariant;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.html.Span;
-import com.vaadin.flow.component.icon.Icon;
-import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.notification.NotificationVariant;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
-import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
-import com.vaadin.flow.spring.data.VaadinSpringDataHelpers;
-import com.vaadin.flow.theme.lumo.LumoUtility;
-import jakarta.annotation.security.PermitAll;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Expression;
-import jakarta.persistence.criteria.Predicate;
-import jakarta.persistence.criteria.Root;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.jpa.domain.Specification;
 
 @PageTitle("Search for a company")
 @Route(value = "grid-with-filters", layout = MainLayout.class)
@@ -89,7 +52,8 @@ public class SearchforacompanyView extends VerticalLayout {
 
         Button searchButton = new Button("Получить данные");
         searchButton.addClickListener(e -> {
-            String jsonData = companyService.getData();
+            String InnParam = searchField.getValue();
+            String jsonData = companyService.getData(InnParam);
             ObjectMapper objectMapper = new ObjectMapper();
 
             try {
